2ed2760eadc933fdc61b5db33f1af6f5b6a2f03d
pangafu <yuanquan@gmail.com>
2021-05-26 06:49:18 +0000

    modify tetris2

--- gym-bz-games/gym_bz_games/envs/tetris2.py
+++ gym-bz-games/gym_bz_games/envs/tetris2.py
@@ -13,23 +13,23 @@ import time
 
 # Reload gym-tetris env for 10 times.(prevent picture chaos)
 class Tetris2CustomState(gym.Wrapper):
-    def __init__(self):
+    def __init__(self, env):
+        super(Tetris2CustomState, self).__init__(env)
         self.grid_height = 23
         self.grid_width = 10
-        
         self.observation_space = spaces.Box(low=0, high=10, shape=(5, self.grid_height, self.grid_width), dtype=np.float32)
 
     def step(self, action):
         state, reward, done, info = self.env.step(action)
-        state = process_frame(state, info)
+        state = self.process_frame(state, info)
         return state, reward, done, info
 
 
     def reset(self):
-        return process_frame(self.env.reset(), {})
+        return self.process_frame(self.env.reset(), {})
 
     # process frame
-    def process_frame(self, action, info):
+    def process_frame(self, state, info):
         # grid_ori    : 1:blank, 4: control, 2: block  => 1: blank, 4: block, 6: control
         # grid_ori_detail :  1: blank, 2:line blank, 3: half blank, 4: block, 5: dead blank
         # grid_drop    : 1: blank, 4: block, 6: control
@@ -54,47 +54,47 @@ class Tetris2CustomState(gym.Wrapper):
                     grid_ori[i, j] = 6
                 elif grid_ori[i, j] == 2:
                     grid_ori[i, j] = 4
-        
+
         # grid_ori_detail :  1: blank, 2:line blank, 3: half blank, 4: block, 5: dead blank
         grid_ori_detail = self.mark_detail(grid_ori, False)
-        
+
         # computer now score
         ori_grid_score, ori_clear_line_num, ori_line_blank, ori_half_holes, ori_dead_holes, ori_border_height = self.computer_detail(grid_ori_detail)
-        
+
         # grid_drop    : 1: blank, 4: block, 6: control
         grid_drop = self.drop_down(grid_ori)
-        
+
         # grid_drop_detail :  1: blank, 2:line blank, 3: half blank, 4: block, 5: dead blank
         grid_drop_detail = self.mark_detail(grid_drop, True)
-        
+
         drop_grid_score, drop_clear_line_num, drop_line_blank, drop_half_holes, drop_dead_holes, drop_border_height = self.computer_detail(grid_drop_detail)
-        
+
         # grid_info  : 0 none, 7 : info, (line<=6)   8 : score up/ can clear, 9: score down
         pos = self.grid_height - 1
-        if drop_grid_score >= ori_grid_score: 
+        if drop_grid_score >= ori_grid_score:
             grid_info[pos, 0] = 8
         else:
             grid_info[pos, 0] = 9
 
-        if drop_clear_line_num > 0: 
+        if drop_clear_line_num > 0:
             grid_info[pos, 1] = 8
 
-        if drop_line_blank > ori_line_blank: 
+        if drop_line_blank > ori_line_blank:
             grid_info[pos, 2] = 9
         else:
             grid_info[pos, 2] = 8
 
-        if drop_half_holes > ori_half_holes: 
+        if drop_half_holes > ori_half_holes:
             grid_info[pos, 3] = 9
         else:
             grid_info[pos, 3] = 8
 
-        if drop_dead_holes > ori_dead_holes: 
+        if drop_dead_holes > ori_dead_holes:
             grid_info[pos, 4] = 9
         else:
             grid_info[pos, 4] = 8
 
-        if drop_border_height > ori_border_height: 
+        if drop_border_height > ori_border_height:
             grid_info[pos, 5] = 9
         else:
             grid_info[pos, 5] = 8
@@ -103,15 +103,15 @@ class Tetris2CustomState(gym.Wrapper):
         info['drop_score'] = [drop_grid_score, drop_clear_line_num, drop_line_blank, drop_half_holes, drop_dead_holes, drop_border_height]
 
         #return state
-        np.array((grid_ori, grid_ori_detail, grid_drop, grid_drop_detail, grid_info))
+        return np.array((grid_ori, grid_ori_detail, grid_drop, grid_drop_detail, grid_info))
 
-            
+
     # grid_drop    : 1: blank, 4: block, 6: control
     def drop_down(self, ori_grid):
         drop_grid = ori_grid.copy()
         drop_end = False
         find_ctrl_obj = False
-        
+
         for i in reversed(range(self.grid_height)):
             if drop_end:
                 break
@@ -120,7 +120,7 @@ class Tetris2CustomState(gym.Wrapper):
                 if ori_grid[i, j] == 6:
                     find_ctrl_obj = True
 
-                    if i >= grid_height-1:
+                    if i >= self.grid_height-1:
                         drop_end = True
                         break
 
@@ -129,17 +129,17 @@ class Tetris2CustomState(gym.Wrapper):
                         break
 
                     else:
-                        drop_grid[i+1, j] = 5
+                        drop_grid[i+1, j] = 6
                         drop_grid[i, j] = 1
 
         if not find_ctrl_obj:
             return drop_grid
-        
+
         if drop_end:
             return ori_grid
         else:
-            return self.drop_down(drop_grid):
-        
+            return self.drop_down(drop_grid)
+
 
     # search grid
     # grid_ori    : 1: blank, 4: block, 6: control
@@ -151,7 +151,7 @@ class Tetris2CustomState(gym.Wrapper):
         if posheight<0 or poswidth<0 or posheight >= grid_height or poswidth >= grid_width:
             return
 
-        if £¨not ctrl_block and ori_grid[posheight, poswidth] == 4£© or (ctrl_block and (ori_grid[posheight, poswidth] == 4 or ori_grid[posheight, poswidth] == 6)) :
+        if (not ctrl_block and ori_grid[posheight, poswidth] == 4) or (ctrl_block and (ori_grid[posheight, poswidth] == 4 or ori_grid[posheight, poswidth] == 6)) :
             detail_grid[posheight, poswidth] = 4      #blocked
             return
 
@@ -173,19 +173,19 @@ class Tetris2CustomState(gym.Wrapper):
     #detail :  1: blank, 2:line blank, 3: half blank, 4: block, 5: dead blank
     def mark_detail(self, ori_grid, ctrl_block):
         detail_grid = np.zeros((self.grid_height, self.grid_width))
-        
+
         # grid_detail: 0: not set, 1: blank, 4: block
         self.search_noblock(ori_grid, detail_grid, 0, 0, ctrl_block)
-        
+
         # grid_detail :  1: blank, 2:line blank, 3: half blank, 4: block, 5: dead blank, 6: control
         for j in range(self.grid_width):
             has_blocked = False
             for i in range(self.grid_height):
-                if £¨not ctrl_block and ori_grid[posheight, poswidth] == 4£© or (ctrl_block and (ori_grid[posheight, poswidth] == 4 or ori_grid[posheight, poswidth] == 6)) :
+                if (not ctrl_block and ori_grid[i, j] == 4) or (ctrl_block and (ori_grid[i, j] == 4 or ori_grid[i, j] == 6)) :
                     has_blocked = True
 
                 if detail_grid[i, j] == 0:
-                    if £¨not ctrl_block and ori_grid[posheight, poswidth] == 4£© or (ctrl_block and (ori_grid[posheight, poswidth] == 4 or ori_grid[posheight, poswidth] == 6)) :
+                    if (not ctrl_block and ori_grid[i, j] == 4) or (ctrl_block and (ori_grid[i, j] == 4 or ori_grid[i, j] == 6)) :
                         detail_grid[i, j] = 4          # blocked  4
                     else:
                         detail_grid[i, j] = 5         # dead hole  5
@@ -196,7 +196,7 @@ class Tetris2CustomState(gym.Wrapper):
                         detail_grid[i, j] == 1         # blank  1
                 elif detail_grid[i, j] == 4:
                     detail_grid[i, j] = 4             # blocked  4
-        
+
         # grid_detail :  2:line blank, 6: control
         for i in range(self.grid_height):
             has_block = False
@@ -205,11 +205,11 @@ class Tetris2CustomState(gym.Wrapper):
                     has_block = True
                     break
 
-            for j in range(grid_width):
+            for j in range(self.grid_width):
                 if detail_grid[i, j] == 1:             # ori blank 1
                     if has_block:
                         detail_grid[i, j] = 2            # line blank 2
-        
+
         return detail_grid
 
 
@@ -223,16 +223,16 @@ class Tetris2CustomState(gym.Wrapper):
         dead_holes = 0
         border_height = 0
         line_all_blank_height = 0
-        
+
         for i in range(self.grid_height):
             line_can_clear = True
             line_all_blank = True
-            
+
             for j in range(self.grid_width):
                 if detail_grid[i, j] != 1:
                     line_all_blank = False
                     break
-            
+
             if line_all_blank:
                 line_all_blank_height += 1
 
@@ -240,11 +240,11 @@ class Tetris2CustomState(gym.Wrapper):
                 if detail_grid[i, j] != 4:
                     line_can_clear = False
                     break
-                    
+
             if line_can_clear:
                 clear_line_num += 1
-                
-            
+
+
             for j in range(self.grid_width):
                 if detail_grid[i, j] == 1:
                     grid_score += 0
@@ -259,21 +259,21 @@ class Tetris2CustomState(gym.Wrapper):
                 elif detail_grid[i, j] == 5:
                     dead_holes += 1
                     grid_score -= 5
-                    
+
         grid_score += 40 * pow(clear_line_num, 4)
-        border_height = sellf.grid_height - line_all_blank_height
-        
+        border_height = self.grid_height - line_all_blank_height
+
         return grid_score, clear_line_num, line_blank, half_holes, dead_holes, border_height
 
-# Reload gym-tetris env for 10 times.(prevent picture chaos)
+# drop down the tetris (origin not drop with step)
 class Tetris2Movedown(gym.Wrapper):
-    def __init__(self):
+    def __init__(self, env):
         super(Tetris2Movedown, self).__init__(env)
         self.curr_frame = 0
 
     def step(self, action):
         self.curr_frame += 1
-        
+
         state, reward, done, info = self.env.step(action)
         last_state = state
         last_reward = reward
@@ -302,7 +302,7 @@ class Tetris2Movedown(gym.Wrapper):
 
     def reset(self):
         self.curr_frame = 0
-        
+
         return self.env.reset()
 
 
@@ -347,8 +347,6 @@ class Tetris2(gym.Env):
         ##if self.last_done:
         #    self.reset()
 
-        self.curr_frame += 1
-
         state, reward, done, info = self.env.step(action)
         self.last_state = state
         self.last_reward = reward
@@ -378,12 +376,25 @@ class Tetris2(gym.Env):
 
     def render(self, mode='human'):
         # if self.last_reward != 0:
-        if True:
+        if mode == 'human':
             print("-------------------------------------------------------------")
-            print(self.last_state)
+            print(self.last_state[0])
             print("Reward  Total:{},  Last:{}".format(self.curr_reward_sum, self.last_reward))
-            print("Done:{}".format(self.last_done))
-            print("Info:{}".format(self.last_info))
+            print("Info     :grid_score, clear_line_num, line_blank, half_holes, dead_holes, border_height")
+            print("Info  ORI:{} ".format(self.last_info["ori_score"]))
+            print("Info DROP:{}".format(self.last_info["drop_score"]))
+            time.sleep(0.3)
+        elif mode == 'detail':
+            print("-------------------------------------------------------------")
+            print(self.last_state[0])
+            print(self.last_state[1])
+            print(self.last_state[2])
+            print(self.last_state[3])
+            print(self.last_state[4])
+            print("Reward  Total:{},  Last:{}".format(self.curr_reward_sum, self.last_reward))
+            print("Info     :grid_score, clear_line_num, line_blank, half_holes, dead_holes, border_height")
+            print("Info  ORI:{} ".format(self.last_info["ori_score"]))
+            print("Info DROP:{}".format(self.last_info["drop_score"]))
             time.sleep(0.3)
         else:
             return
